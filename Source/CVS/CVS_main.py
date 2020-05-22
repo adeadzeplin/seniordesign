15f8069b2f98581cab369345a8db8fd05c03771a
Stanyslav Borsh <stan.borsh@gmail.com>
2020-05-22 11:45:22 -0400

    updating current build,

--- Source/CVS/CVS_main.py
+++ Source/CVS/CVS_main.py
@@ -9,21 +9,62 @@ def CVS():
     CVS_circuit_creation.create_circuit_outputs(listOfGates)
 
     # make XOR gate with A B inputs
-    listOfGates.append(CVS_gate_class.Gate(CVS_gate_class.GateType.AND))
-    # listOfGates.append(gate_class.Gate(gate_class.GateType.XOR))
+    listOfGates.append(CVS_gate_class.Gate(CVS_gate_class.GateType.XOR))
+    #listOfGates.append(CVS_gate_class.Gate(CVS_gate_class.GateType.AND))
+    # listOfGates.append(CVS_gate_class.Gate(CVS_gate_class.GateType.AND))
+    # listOfGates.append(CVS_gate_class.Gate(CVS_gate_class.GateType.XOR))
+    # listOfGates.append(CVS_gate_class.Gate(CVS_gate_class.GateType.XOR))
 
     # connect gates
+# one gate
     CVS_circuit_creation.Output_to_Input(listOfGates, 0, 3)
-    CVS_circuit_creation.Output_to_Input(listOfGates, 1, 4)
+    CVS_circuit_creation.Output_to_Input(listOfGates, 1, 3)
     CVS_circuit_creation.Output_to_Input(listOfGates, 3, 2)
 
-    # circuit_creation.Output_to_Input(listOfGates, 0, 4)
-    # circuit_creation.Output_to_Input(listOfGates, 1, 5)
-    # circuit_creation.Output_to_Input(listOfGates, 6, 2)
+# half adder
+#     CVS_circuit_creation.Output_to_Input(listOfGates, 0, 4)
+#     CVS_circuit_creation.Output_to_Input(listOfGates, 1, 5)
+#     CVS_circuit_creation.Output_to_Input(listOfGates, 6, 2)
+#
+#     CVS_circuit_creation.Output_to_Input(listOfGates, 0, 7)
+#     CVS_circuit_creation.Output_to_Input(listOfGates, 1, 8)
+#     CVS_circuit_creation.Output_to_Input(listOfGates, 9, 3)
+
+    # CVS_circuit_creation.Output_to_Input(listOfGates, 0, 4)
+    # CVS_circuit_creation.Output_to_Input(listOfGates, 1, 4)
+    # CVS_circuit_creation.Output_to_Input(listOfGates, 4, 2)
     #
-    # circuit_creation.Output_to_Input(listOfGates, 0, 7)
-    # circuit_creation.Output_to_Input(listOfGates, 1, 8)
-    # circuit_creation.Output_to_Input(listOfGates, 9, 3)
+    # CVS_circuit_creation.Output_to_Input(listOfGates, 0, 5)
+    # CVS_circuit_creation.Output_to_Input(listOfGates, 1, 5)
+    # CVS_circuit_creation.Output_to_Input(listOfGates, 5, 3)
+
+
+
+# full adder
+    #xor 5
+    # CVS_circuit_creation.Output_to_Input(listOfGates, 0, 5)
+    # CVS_circuit_creation.Output_to_Input(listOfGates, 1, 6)
+    # CVS_circuit_creation.Output_to_Input(listOfGates, 7, 14)
+    #
+    # #and 6
+    # CVS_circuit_creation.Output_to_Input(listOfGates, 0, 8)
+    # CVS_circuit_creation.Output_to_Input(listOfGates, 1, 9)
+    # CVS_circuit_creation.Output_to_Input(listOfGates, 10, 17)
+    #
+    # #and 7
+    # CVS_circuit_creation.Output_to_Input(listOfGates, 2, 12)
+    # CVS_circuit_creation.Output_to_Input(listOfGates, 7, 11)
+    # CVS_circuit_creation.Output_to_Input(listOfGates, 13, 18)
+    #
+    # #xor 8
+    # CVS_circuit_creation.Output_to_Input(listOfGates, 2, 15)
+    # CVS_circuit_creation.Output_to_Input(listOfGates, 7, 14)
+    # CVS_circuit_creation.Output_to_Input(listOfGates, 16, 3)
+    #
+    # #xor 9
+    # CVS_circuit_creation.Output_to_Input(listOfGates, 13, 17)
+    # CVS_circuit_creation.Output_to_Input(listOfGates, 10, 18)
+    # CVS_circuit_creation.Output_to_Input(listOfGates, 19, 4)
 
     ParserOutputs = CVS_parser.organizing(listOfGates)
     CrawlerOut = CVS_parser.circuitCrawling(listOfGates, ParserOutputs[1])
--- Source/CVS/CVS_parser.py
+++ Source/CVS/CVS_parser.py
@@ -23,50 +23,82 @@ def organizing(listOfGates):
     return inputTTList, TToutputs
 
 def circuitCrawling(listOFGates,TToutputs):
-    temp_path = []
+    temp_output_id = []
     temp_input_id = []
+    temp_gate_id = []
     for i in listOFGates:   #start from output
         if i.type == 1:
-            for j in range(len(listOFGates)):
-                try:
-                    print(i.inputs[j].mated_to)
-                    temp_path.append(i)
-                except:
-                    print('fail')
+            #for j in range(len(listOFGates)):
+            try:
+                #print(i.inputs[j].mated_to)
+                temp_output_id.append(i)
+            except:
+                print('fail')
         elif i.type == 0:             #if input is reached, by this point a list of gates should be found
-            for j in range(len(listOFGates)):
-                try:
-                    print(i.outputs[j].mated_to)
-                    temp_input_id.append(i)
-                except:
+            #for j in range(len(listOFGates)):
+            try:
+                #print(i.outputs[j].mated_to)
+                temp_input_id.append(i)
+            except:
                     print('fail')
         else:                         #rest of gates added here
-            pass
+            try:
+                # print(i.outputs[j].mated_to)
+                temp_gate_id.append(i)
+            except:
+                print('fail')
 
-    print(temp_path)
+    print(temp_output_id)
     print(temp_input_id)
+    print(temp_gate_id)
 
-    crawlerOutput = [temp_path, temp_input_id]
+    crawlerOutput = [temp_output_id, temp_input_id,temp_gate_id]
     return crawlerOutput
     #Look at gates inputs
 
 def convertNumtoWord(gate_id):
-    if gate_id == 2: return "AND"
-    elif gate_id == 3: return "OR"
-    elif gate_id == 4: return "NOT"
-    elif gate_id == 5: return "NAND"
-    elif gate_id == 6: return "NOR"
-    elif gate_id == 7: return "XOR"
+    if gate_id == 2: return "and"
+    elif gate_id == 3: return "or"
+    elif gate_id == 4: return "not"
+    elif gate_id == 5: return "nand"
+    elif gate_id == 6: return "nor"
+    elif gate_id == 7: return "xor"
 
 def printTable(inputTTList, CrawlerOut):
     #print(CrawlerOut)
     #format crawler out
-    input1, operand, input2 = "","",""
-    expression = "%s %s %s"% (input1, operand, input2)
+    #input1, operand, input2 = "","",""
+    #expression = "%s %s %s"% (input1, operand, input2)
     formattedLabel = []
-    for i in CrawlerOut[0]:
-        print(i.gate_id)
-        gateName = convertNumtoWord(i.gate_id)
+    #print(CrawlerOut[0])
+    for i in CrawlerOut[2]:
+        if i.type == 4:
+            # print(i.type)
+            gateName = convertNumtoWord(i.type)
+            # print(gateName)
+            # print(inputTTList)
+            input1 = inputTTList[0]
+            operand = gateName
+            expression = "%s %s" % (operand, input1)
+            print(expression)
+            formattedLabel.append(expression)
+        else:
+            # print((list(map(str, list(set(formattedLabel))))))
+            formattedLabel = (list(map(str, list(set(formattedLabel)))))
+            #print(i.type)
+            gateName = convertNumtoWord(i.type)
+            print(gateName)
+            #print(inputTTList)
+            input1 = inputTTList[0]
+            input2 = inputTTList[1]
+            operand = gateName
+            expression = "%s %s %s" % (input1, operand, input2)
+            #print(expression)
+            formattedLabel.append(expression)
+
+        print(formattedLabel)
+        #print((list(map(str, list(set(formattedLabel))))))
+        #formattedLabel = (list(map(str, list(set(formattedLabel)))))
 
 
     table = ttg.Truths((list(map(str, list(set(inputTTList))))),formattedLabel)
