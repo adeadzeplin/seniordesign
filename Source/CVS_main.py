00e5b954d5b7b3825cdd8ebe43b48fa7f903e2f7
stan.borsh@gmail.com <Pinnappleman33>
2020-05-22 22:58:44 -0400

    fixed bug where if only one gate is connected in the circuit vs more than one

--- Source/CVS_main.py
+++ Source/CVS_main.py
@@ -11,60 +11,53 @@ def CVS():
     # make XOR gate with A B inputs
     listOfGates.append(CVS_gate_class.Gate(CVS_gate_class.GateType.XOR))
     listOfGates.append(CVS_gate_class.Gate(CVS_gate_class.GateType.AND))
-    listOfGates.append(CVS_gate_class.Gate(CVS_gate_class.GateType.AND))
-    listOfGates.append(CVS_gate_class.Gate(CVS_gate_class.GateType.XOR))
-    listOfGates.append(CVS_gate_class.Gate(CVS_gate_class.GateType.XOR))
+    # listOfGates.append(CVS_gate_class.Gate(CVS_gate_class.GateType.AND))
+    # listOfGates.append(CVS_gate_class.Gate(CVS_gate_class.GateType.XOR))
+    # listOfGates.append(CVS_gate_class.Gate(CVS_gate_class.GateType.XOR))
 
     # connect gates
-# one gate
-#     CVS_circuit_creation.Output_to_Input(listOfGates, 0, 3)
-#     CVS_circuit_creation.Output_to_Input(listOfGates, 1, 3)
-#     CVS_circuit_creation.Output_to_Input(listOfGates, 3, 2)
+    #one gate
+    CVS_circuit_creation.Output_to_Input(listOfGates, 0, 3)
+    CVS_circuit_creation.Output_to_Input(listOfGates, 1, 3)
+    CVS_circuit_creation.Output_to_Input(listOfGates, 3, 2)
 
 # half adder
-#     CVS_circuit_creation.Output_to_Input(listOfGates, 0, 4)
-#     CVS_circuit_creation.Output_to_Input(listOfGates, 1, 5)
-#     CVS_circuit_creation.Output_to_Input(listOfGates, 6, 2)
-#
-#     CVS_circuit_creation.Output_to_Input(listOfGates, 0, 7)
-#     CVS_circuit_creation.Output_to_Input(listOfGates, 1, 8)
-#     CVS_circuit_creation.Output_to_Input(listOfGates, 9, 3)
-#
-#     CVS_circuit_creation.Output_to_Input(listOfGates, 0, 4)
-#     CVS_circuit_creation.Output_to_Input(listOfGates, 1, 4)
-#     CVS_circuit_creation.Output_to_Input(listOfGates, 4, 2)
-#
-#     CVS_circuit_creation.Output_to_Input(listOfGates, 0, 5)
-#     CVS_circuit_creation.Output_to_Input(listOfGates, 1, 5)
-#     CVS_circuit_creation.Output_to_Input(listOfGates, 5, 3)
+    #xor 4
+    CVS_circuit_creation.Output_to_Input(listOfGates, 0, 4)
+    CVS_circuit_creation.Output_to_Input(listOfGates, 1, 4)
+    CVS_circuit_creation.Output_to_Input(listOfGates, 4, 2)
+    #and 5
+    CVS_circuit_creation.Output_to_Input(listOfGates, 0, 5)
+    CVS_circuit_creation.Output_to_Input(listOfGates, 1, 5)
+    CVS_circuit_creation.Output_to_Input(listOfGates, 5, 3)
 
 
 
 # full adder
-    #xor 5
-    CVS_circuit_creation.Output_to_Input(listOfGates, 0, 5)
-    CVS_circuit_creation.Output_to_Input(listOfGates, 1, 5)
-    CVS_circuit_creation.Output_to_Input(listOfGates, 5, 18)
-
-    #and 6
-    CVS_circuit_creation.Output_to_Input(listOfGates, 0, 6)
-    CVS_circuit_creation.Output_to_Input(listOfGates, 1, 6)
-    CVS_circuit_creation.Output_to_Input(listOfGates, 6, 9)
-
-    #and 7
-    CVS_circuit_creation.Output_to_Input(listOfGates, 2, 7)
-    CVS_circuit_creation.Output_to_Input(listOfGates, 5, 7)
-    CVS_circuit_creation.Output_to_Input(listOfGates, 7, 9)
-
-    #xor 8
-    CVS_circuit_creation.Output_to_Input(listOfGates, 5, 8)
-    CVS_circuit_creation.Output_to_Input(listOfGates, 2, 8)
-    CVS_circuit_creation.Output_to_Input(listOfGates, 8, 3)
-
-    #xor 9
-    CVS_circuit_creation.Output_to_Input(listOfGates, 7, 9)
-    CVS_circuit_creation.Output_to_Input(listOfGates, 6, 9)
-    CVS_circuit_creation.Output_to_Input(listOfGates, 9, 4)
+#     #xor 5
+#     CVS_circuit_creation.Output_to_Input(listOfGates, 0, 5)
+#     CVS_circuit_creation.Output_to_Input(listOfGates, 1, 5)
+#     CVS_circuit_creation.Output_to_Input(listOfGates, 5, 18)
+#
+#     #and 6
+#     CVS_circuit_creation.Output_to_Input(listOfGates, 0, 6)
+#     CVS_circuit_creation.Output_to_Input(listOfGates, 1, 6)
+#     CVS_circuit_creation.Output_to_Input(listOfGates, 6, 9)
+#
+#     #and 7
+#     CVS_circuit_creation.Output_to_Input(listOfGates, 2, 7)
+#     CVS_circuit_creation.Output_to_Input(listOfGates, 5, 7)
+#     CVS_circuit_creation.Output_to_Input(listOfGates, 7, 9)
+#
+#     #xor 8
+#     CVS_circuit_creation.Output_to_Input(listOfGates, 5, 8)
+#     CVS_circuit_creation.Output_to_Input(listOfGates, 2, 8)
+#     CVS_circuit_creation.Output_to_Input(listOfGates, 8, 3)
+#
+#     #xor 9
+#     CVS_circuit_creation.Output_to_Input(listOfGates, 7, 9)
+#     CVS_circuit_creation.Output_to_Input(listOfGates, 6, 9)
+#     CVS_circuit_creation.Output_to_Input(listOfGates, 9, 4)
 
     ParserOutputs = CVS_parser.organizing(listOfGates)
     CrawlerOut = CVS_parser.circuitCrawling(listOfGates)
