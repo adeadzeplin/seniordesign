b3fdbc17488909a2806d2abc96f8bcd86553ef21
Stanyslav Borsh <stan.borsh@gmail.com>
2020-05-25 22:32:26 -0400

    added desired table and output tbale comparison

--- Source/CVS_/CVS_main.py
+++ Source/CVS_/CVS_main.py
@@ -11,10 +11,10 @@ def CVS():
     # make XOR gate with A B inputs
     listOfGates.append(CVS_gate_class.Gate(CVS_gate_class.GateType.XOR))
     listOfGates.append(CVS_gate_class.Gate(CVS_gate_class.GateType.AND))
-    # listOfGates.append(CVS_gate_class.Gate(CVS_gate_class.GateType.AND))
-    # listOfGates.append(CVS_gate_class.Gate(CVS_gate_class.GateType.XOR))
-    # listOfGates.append(CVS_gate_class.Gate(CVS_gate_class.GateType.XOR))
-    # listOfGates.append(CVS_gate_class.Gate(CVS_gate_class.GateType.NOT))
+    listOfGates.append(CVS_gate_class.Gate(CVS_gate_class.GateType.AND))
+    listOfGates.append(CVS_gate_class.Gate(CVS_gate_class.GateType.XOR))
+    listOfGates.append(CVS_gate_class.Gate(CVS_gate_class.GateType.XOR))
+    #listOfGates.append(CVS_gate_class.Gate(CVS_gate_class.GateType.NOT))
 
     # connect gates
     #one gate
@@ -29,41 +29,45 @@ def CVS():
 
 # half adder
     #xor 4
-    CVS_circuit_creation.Output_to_Input(listOfGates, 0, 4)
-    CVS_circuit_creation.Output_to_Input(listOfGates, 1, 4)
-    CVS_circuit_creation.Output_to_Input(listOfGates, 4, 2)
-    #and 5
-    CVS_circuit_creation.Output_to_Input(listOfGates, 0, 5)
-    CVS_circuit_creation.Output_to_Input(listOfGates, 1, 5)
-    CVS_circuit_creation.Output_to_Input(listOfGates, 5, 3)
+    # CVS_circuit_creation.Output_to_Input(listOfGates, 0, 4)
+    # CVS_circuit_creation.Output_to_Input(listOfGates, 1, 4)
+    # CVS_circuit_creation.Output_to_Input(listOfGates, 4, 2)
+    # #and 5
+    # CVS_circuit_creation.Output_to_Input(listOfGates, 0, 5)
+    # CVS_circuit_creation.Output_to_Input(listOfGates, 1, 5)
+    # CVS_circuit_creation.Output_to_Input(listOfGates, 5, 3)
+    #
+    # ogCircuitOutput = [[0,1,1,0],[0,0,0,1]]
 
 
 
 # full adder
 #     #xor 5
-#     CVS_circuit_creation.Output_to_Input(listOfGates, 0, 5)
-#     CVS_circuit_creation.Output_to_Input(listOfGates, 1, 5)
-#     CVS_circuit_creation.Output_to_Input(listOfGates, 5, 18)
-#
-#     #and 6
-#     CVS_circuit_creation.Output_to_Input(listOfGates, 0, 6)
-#     CVS_circuit_creation.Output_to_Input(listOfGates, 1, 6)
-#     CVS_circuit_creation.Output_to_Input(listOfGates, 6, 9)
-#
-#     #and 7
-#     CVS_circuit_creation.Output_to_Input(listOfGates, 2, 7)
-#     CVS_circuit_creation.Output_to_Input(listOfGates, 5, 7)
-#     CVS_circuit_creation.Output_to_Input(listOfGates, 7, 9)
-#
-#     #xor 8
-#     CVS_circuit_creation.Output_to_Input(listOfGates, 5, 8)
-#     CVS_circuit_creation.Output_to_Input(listOfGates, 2, 8)
-#     CVS_circuit_creation.Output_to_Input(listOfGates, 8, 3)
-#
-#     #xor 9
-#     CVS_circuit_creation.Output_to_Input(listOfGates, 7, 9)
-#     CVS_circuit_creation.Output_to_Input(listOfGates, 6, 9)
-#     CVS_circuit_creation.Output_to_Input(listOfGates, 9, 4)
+    CVS_circuit_creation.Output_to_Input(listOfGates, 0, 5)
+    CVS_circuit_creation.Output_to_Input(listOfGates, 1, 5)
+    CVS_circuit_creation.Output_to_Input(listOfGates, 5, 18)
+
+    #and 6
+    CVS_circuit_creation.Output_to_Input(listOfGates, 0, 6)
+    CVS_circuit_creation.Output_to_Input(listOfGates, 1, 6)
+    CVS_circuit_creation.Output_to_Input(listOfGates, 6, 9)
+
+    #and 7
+    CVS_circuit_creation.Output_to_Input(listOfGates, 2, 7)
+    CVS_circuit_creation.Output_to_Input(listOfGates, 5, 7)
+    CVS_circuit_creation.Output_to_Input(listOfGates, 7, 9)
+
+    #xor 8
+    CVS_circuit_creation.Output_to_Input(listOfGates, 5, 8)
+    CVS_circuit_creation.Output_to_Input(listOfGates, 2, 8)
+    CVS_circuit_creation.Output_to_Input(listOfGates, 8, 3)
+
+    #xor 9
+    CVS_circuit_creation.Output_to_Input(listOfGates, 7, 9)
+    CVS_circuit_creation.Output_to_Input(listOfGates, 6, 9)
+    CVS_circuit_creation.Output_to_Input(listOfGates, 9, 4)
+
+    ogCircuitOutput = [[0, 1, 1, 0, 1, 0, 0, 1], [0, 0, 0, 1, 0, 1, 1, 1]]
 
     #intial circuit connection check
     Circuit_Errors = CVS_circuit_calculations.circuit_connection_check(listOfGates)
@@ -72,7 +76,9 @@ def CVS():
         ParserOutputs = CVS_parser.organizing(listOfGates)
         CrawlerOut = CVS_parser.circuitCrawling(listOfGates)
         returnValue = CVS_parser.printTable(ParserOutputs[0], CrawlerOut)
-        print(returnValue)
+        print("Circuit Output:",returnValue)
+        circuitPercentSame = CVS_circuit_calculations.circuit_output_compare(returnValue,ogCircuitOutput)
+        print("Percent Circuit Output is Equal to OG:", circuitPercentSame)
     else:
         print(Circuit_Errors)
 
