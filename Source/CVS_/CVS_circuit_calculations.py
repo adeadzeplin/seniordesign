19add58ef0e6c7caff6fc190f05bef6edfedbfc8
stan.borsh@gmail.com <Pinnappleman33>
2020-06-05 14:24:27 -0400

    clean up code, removed unnecessary/unused code Commented some code as well

--- Source/CVS_/CVS_circuit_calculations.py
+++ Source/CVS_/CVS_circuit_calculations.py
@@ -1,6 +1,7 @@
 import enum
 import numpy as np
 
+
 def Total_Power():
     pass
 
@@ -8,83 +9,84 @@ def Total_Power():
 def Total_Delay():
     pass
 
+
 def circuit_connection_check(listofallgates):
     input_gate_counter = 0
     logic_gate_counter = 0
     output_gate_counter = 0
 
-    #checks if gates exist
+    # checks if gates exist
     for gate in listofallgates:
         if gate.type == 0:
-            input_gate_counter +=1
+            input_gate_counter += 1
         elif gate.type == 1:
-            output_gate_counter +=1
+            output_gate_counter += 1
         else:
-            logic_gate_counter +=1
+            logic_gate_counter += 1
 
     if input_gate_counter == 0:
         return circuit_errors.ERROR_MISSING_INPUT
     elif logic_gate_counter == 0:
-        return  circuit_errors.ERROR_MISSING_LOGIC
+        return circuit_errors.ERROR_MISSING_LOGIC
     elif output_gate_counter == 0:
-        return  circuit_errors.ERROR_MISSING_OUTPUT
+        return circuit_errors.ERROR_MISSING_OUTPUT
 
-
-    #checks is each gate has any inputs and outputs
+    # checks is each gate has any inputs and outputs
     for gate in listofallgates:
         for j in range(len(gate.outputs)):
             if gate.type == 0:
-                if len(gate.outputs[j].mated_to) == 0 :
+                if len(gate.outputs[j].mated_to) == 0:
                     return circuit_errors.ERROR_CIRCUIT_INPUT
                 else:
-                    pass #print(gate.gate_id,gate.outputs[j].mated_to)
+                    pass  # print(gate.gate_id,gate.outputs[j].mated_to)
             elif gate.type != 1:
                 if len(gate.outputs[j].mated_to) == 0 or len(gate.inputs[j].mated_to) > 2:
                     return circuit_errors.ERROR_GATE
                 else:
-                    pass #print(gate.gate_id,gate.inputs[j].mated_to,gate.outputs[j].mated_to)
+                    pass  # print(gate.gate_id,gate.inputs[j].mated_to,gate.outputs[j].mated_to)
 
         for k in range(len(gate.inputs)):
             if gate.type == 1:
                 if len(gate.inputs[k].mated_to) == 0 or len(gate.inputs[k].mated_to) > 1:
                     return circuit_errors.ERROR_CIRCUIT_OUTPUT
                 else:
-                    pass #print(gate.gate_id,gate.inputs[k].mated_to)
+                    pass  # print(gate.gate_id,gate.inputs[k].mated_to)
+
 
 class circuit_errors(enum.Enum):
-    #Circuit Error codes
-    ERROR_CIRCUIT_INPUT =1
-    ERROR_CIRCUIT_OUTPUT =2
-    ERROR_GATE = 3
-    ERROR_MISSING_INPUT = 4
-    ERROR_MISSING_LOGIC = 5
-    ERROR_MISSING_OUTPUT = 6
+    # Circuit Error codes
+    ERROR_CIRCUIT_INPUT = 10
+    ERROR_CIRCUIT_OUTPUT = 20
+    ERROR_GATE = 30
+    ERROR_MISSING_INPUT = 40
+    ERROR_MISSING_LOGIC = 50
+    ERROR_MISSING_OUTPUT = 60
+
 
 def circuit_output_compare(circuitOutput, ogOutput):
-    counterCorrect = 0
     if circuitOutput == ogOutput:
         return 100
     else:
-        #see how much is correct
+        # see how much is correct
         a = np.array(circuitOutput)
         b = np.array(ogOutput)
-        error = np.mean(a!=b)
+        error = np.mean(a != b)
         return error
 
 
-def table_column_get(tableInput_TableOut,circuitInput):
+def table_column_get(tableInput_TableOut, circuitInput):
     tableColumn = []
     for q in range(len(tableInput_TableOut)):
-        if len(tableInput_TableOut) ==1:
-            for k in range(1, len(tableInput_TableOut) +2):
-                #print(k,tableInput_TableOut[q][str(q)][k])
+        if len(tableInput_TableOut) == 1:
+            for k in range(1, len(tableInput_TableOut) + 2):
+                # print(k,tableInput_TableOut[q][str(q)][k])
                 tableColumn.append(tableInput_TableOut[q][str(q)][k])
             temp = tableColumn
             tableColumn = []
             circuitInput.append(temp)
 
-        elif len(tableInput_TableOut)**2 <=4:
-            for k in range(1, len(tableInput_TableOut) ** 2+1):
+        elif len(tableInput_TableOut) ** 2 <= 4:
+            for k in range(1, len(tableInput_TableOut) ** 2 + 1):
                 # print(k,tableInput_TableOut[q][str(q)][k])
                 tableColumn.append(tableInput_TableOut[q][str(q)][k])
             temp = tableColumn
@@ -100,38 +102,39 @@ def table_column_get(tableInput_TableOut,circuitInput):
     print(circuitInput)
     return circuitInput
 
-def table_output(a,b, gatetype):
-    #print(a,b,gatetype)
+
+def table_output(a, b, gatetype):
+    # print(a,b,gatetype)
     output = []
 
-    if gatetype == 2 :
+    if gatetype == 2:
         for i in range(len(a)):
             if a[i] == 1 and b[i] == 1:
                 output.append(1)
             else:
                 output.append(0)
-    elif gatetype == 3 :
+    elif gatetype == 3:
         for i in range(len(a)):
-            if a[i]==1 or b[i]==1:
+            if a[i] == 1 or b[i] == 1:
                 output.append(1)
             else:
                 output.append(0)
-    elif gatetype == 4 :
+    elif gatetype == 4:
         for i in range(len(a)):
             output.append(not a[i])
-    elif gatetype == 5 :
+    elif gatetype == 5:
         for i in range(len(a)):
             if a[i] == 1 and b[i] == 1:
                 output.append(0)
             else:
                 output.append(1)
-    elif gatetype == 6 :
+    elif gatetype == 6:
         for i in range(len(a)):
             if a[i] == 1 or b[i] == 1:
                 output.append(0)
             else:
                 output.append(1)
-    elif gatetype == 7 :
+    elif gatetype == 7:
         for i in range(len(a)):
             if a[i] == b[i]:
                 output.append(0)
@@ -139,7 +142,6 @@ def table_output(a,b, gatetype):
                 output.append(1)
 
     return output
-
 
     # AND = 2
     # OR = 3
