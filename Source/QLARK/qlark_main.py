14484fcb19768720fe185b2d33e1995d9e9005e7
smithdepazd <smithdepazd@wit.edu>
2020-06-19 17:43:36 -0400

    Implemented MultiThreading -RunBest() is broken IDK why

--- Source/QLARK/qlark_main.py
+++ Source/QLARK/qlark_main.py
@@ -1,10 +1,12 @@
-from QLARK.qlark_class import Qlark
+import QLARK.qlark_threading as qt
 
 
 if __name__ == '__main__':
 
-    desired_truthtable =  [[0,1,1,0],[0,0,0,1]]
+    desired_truthtable =  [[0,1,1,0],[0,0,0,1]] # Half Adder circuit Truth Table
 
-    Qlearingai = Qlark(desired_truthtable)
-    Qlearingai.train()
-    Qlearingai.runBest()
+    Num_training_set_p_thread = 1               # Number of training sets of 10,000 per thread
+    Number_of_threads = 4                       # number of threads to instantiate
+    qt.Needlethreading(Number_of_threads, desired_truthtable, Num_training_set_p_thread)
+
+
